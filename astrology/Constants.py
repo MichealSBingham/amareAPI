2a161d14b90820234c17256d67ee34845c941df7
Micheal Bingham <micheal@findamare.com>
2022-08-05 17:27:46 -0400

    added totalSynastry

--- astrology/Constants.py
+++ astrology/Constants.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pytz
 
 loc=Location(latitude=float(32.2988), longitude=float(-90.1848))
+jacksonMS = loc 
 micheal = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(932604720), known_time=True, hometown=loc)
 micheal.natal()
 
@@ -27,8 +28,30 @@ sahil.natal()
 fahid = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(fahidTimeStamp), known_time=True, hometown=atlanta)
 fahid.natal()
 
+westCovaCA=Location(latitude=float(34.0686), longitude=float(-117.9390))
+zyla = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(847335540), known_time=True, hometown=westCovaCA)
+zyla.natal()
 
+zylasPerson=Location(latitude=float(34.7304), longitude=float(-86.5861))
+zylasPerson = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(611583300), known_time=True, hometown=westCovaCA)
+zylasPerson.natal()
 
+kelle = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(335836800), known_time=True, hometown=loc)
+kelle.natal()
+
+maurice = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(255355200), known_time=True, hometown=loc)
+maurice.natal()
+
+boo = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(109872000), known_time=True, hometown=loc)
+boo.natal()
+
+oscarbday = -569656800
+oscar = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(oscarbday), known_time=False, hometown=loc)
+oscar.natal()
+
+ouidsabday = -571212000
+ouida = User(do_not_fetch=True,  birthday = datetime.utcfromtimestamp(ouidsabday), known_time=False, hometown=loc)
+ouida.natal()
 
 
 # Returns a list of the Capricon Suns in ALL degrees
