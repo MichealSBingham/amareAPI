fb66269389d547c8212a003e15fe287c42b50a86
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2023-10-24 20:52:50 -0400

    changing model for one of the algos

--- prompts/astrology_traits_generator.py
+++ prompts/astrology_traits_generator.py
@@ -11,14 +11,14 @@ from prompts.constants import *
 class PlacementInterpretationsGenerator:
     
     def __init__(self):
-        self.model = "gpt-3.5-turbo-16k"
+        self.model = "gpt-3.5-turbo"
         self.instructions = PREDICTING_PLANETARY_PLACEMENT
         self.messages = []
 
     # example: male, sun, cancer, 7th. be sure the house number 
     def _format_request_message(self,  gender, planet, sign, house=''):
         if not house or house.isspace():
-            message = f"I'm a {gender} with {planet} in {sign} in the {house} House. Tell me about myself. Do NOT mention the name of the placement until the end of the reading, towards the end you can speak more about the reasoning behind it such as the planet, sign, and house, but in the beginning just tell me my reading nothing else. "
+            message = f"I'm a {gender} with {planet} in {sign} in the {house} House. Tell me about myself.  "
         else: 
             message = f"I'm a {gender} with {planet} in {sign}. Tell me about myself. Do NOT mention the name of the placement until the end of the reading, towards the end you can speak more about the reasoning behind it such as the planet, sign, and house, but in the beginning just tell me my reading nothing else. "
         return message.strip()
@@ -37,7 +37,7 @@ class PlacementInterpretationsGenerator:
         response = openai.ChatCompletion.create(
                 model=self.model,
                 messages=outbound_messages, 
-                temperature=0.45, # adjust this value as needed
+                temperature=1, # adjust this value as needed
                 max_tokens=555 
             )
         response_content = response.choices[0].message['content'].strip()
