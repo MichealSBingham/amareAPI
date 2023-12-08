02b695f1e8b61a806face934c7d67dbea75303e5
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2023-12-08 12:15:21 -0500

    sending messages from dasha can do this from the backend now

--- Messaging/streamBackend.py
+++ Messaging/streamBackend.py
@@ -1,19 +1,82 @@
-# pip install stream-chat
-import stream_chat
+# streamBackend.py
+
+from stream_chat import StreamChat
+
+# Set up Stream Chat client
+api_key = "92jyyxebed2m"
+api_secret = "zh8xege9catts7pfdageqsfc6vhgwttqedrsa9kvfyxjmucqst4wn97ycgn765wc"
+chat_region = "us-east"
+
+chat_client = StreamChat(api_key, api_secret, region=chat_region)
+dasha_image_url = "https://firebasestorage.googleapis.com/v0/b/findamare.appspot.com/o/0_1.png?alt=media&token=c4e66adc-9ba3-4acf-b6bd-f7e2ee167237"
+
+# Create Dasha's user
+def create_dasha_user():
+    user_id = "dasha"
+    profile_image = "https://firebasestorage.googleapis.com/v0/b/findamare.appspot.com/o/0_1.png?alt=media&token=c4e66adc-9ba3-4acf-b6bd-f7e2ee167237"
+
+    try:
+        chat_client.upsert_user({"id": user_id, "role": "admin", "image": dasha_image_url, "name": "Dasha"})
+
+       
+
+        print(f"User Dasha created")
+    except Exception as e:
+        print(f"Error creating Dasha's user: {e}")
+
+# Send a welcome message to a specific user
+def send_welcome_message(user_id):
+    channel_type = "messaging"
+
+    channel_id = f"{user_id}-dasha"
+
+    try:
+        
+
+        channel = chat_client.channel(channel_type, channel_id)
+        channel.create(user_id)
+        channel.update({"name": "Dasha", "image": dasha_image_url})
 
 
-STREAM_KEY = "6vb87hptvk7d"
-STREAM_SECRET = "khx3uf4rtnyr23shme3u86dqe4m5veh8sn5zkhwuuypjd37jw4yh2dyn5xas7cy6"
+        channel.add_members(["dasha", user_id]) #IF Necessary 
 
-server_client = stream_chat.StreamChat(api_key=STREAM_KEY, api_secret=STREAM_SECRET)
+        
+        # Send the welcome message
+        message = {"text": "Hey, I'm Dasha! 💁🏼‍♀️ Ask me anything about your birth chart , astrology, relationships 💖 , and dating 👩🏼‍❤️‍💋‍👨🏾! I can also give readings-- only for you though! 😉"}
+        channel.send_message(message, "dasha")
+        print(f"Welcome message sent to user {user_id}.")
+    except Exception as e:
+        print(f"Error sending welcome message: {e}")
 
+# Send a message to a specific user
+def send_message_to_user(user_id, message_text):
+    channel_type = "messaging"
+    channel_id = f"{user_id}-dasha"
 
+    try:
+   
+        channel = chat_client.channel(channel_type, channel_id)
+        
+        
+        # Send the message
+        message = {
+    "text": message_text,
+            }
 
-def get_token_from_user_id(user_id):
-    """This returns a token that the frontend can use to authenticate with the backend to send messages. 
-    This token is generated from the user_id and has no expiration date."""
-    return server_client.create_token(user_id)
+        channel.send_message(message, "dasha")
 
+        print(f"Message sent to user {user_id}: {message_text}")
+    except Exception as e:
+        print(f"Error sending message to user {user_id}: {e}")
 
+"""
+if __name__ == "__main__":
+    create_dasha_user()
 
-#TODO: Make a method to revoke the token when the user logs out.
+    # Replace 'example_user_id' with the actual user ID you want to send the welcome message to
+    send_welcome_message("example_user_id")
+
+    # Replace 'example_user_id' with the actual user ID you want to send a test message to
+    send_message_to_user("example_user_id", "Hello there!")
+
+"""
